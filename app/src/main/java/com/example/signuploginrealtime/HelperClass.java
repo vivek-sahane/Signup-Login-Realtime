a1cd82d163e1af4648005829be4ebbd2f16842d0
vivek-sahane <viveksahane1528@gmail.com>
2024-11-08 20:56:17 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/signuploginrealtime/HelperClass.java
@@ -0,0 +1,52 @@
+package com.example.signuploginrealtime;
+
+public class HelperClass {
+    //declare all the variables that we are declaring to store in firedata base
+    String name;
+    String email;
+    String username;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public HelperClass(String email, String name, String username, String password) {
+        this.email = email;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    String password;
+
+    public HelperClass() {
+    }
+}
---
+++ app/src/main/java/com/example/signuploginrealtime/SignupActivity.java
@@ -0,0 +1,73 @@
+package com.example.signuploginrealtime;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignupActivity extends AppCompatActivity {
+
+    EditText signupName , signupEmail,signupUsername, signupPassword;
+    TextView loginRedirectText;
+    Button signupButton;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_signup);
+
+        //Initialise all the values
+        signupName = findViewById(R.id.signup_name);
+        signupEmail = findViewById(R.id.signup_email);
+        signupUsername = findViewById(R.id.signup_username);
+        signupPassword = findViewById(R.id.signup_password);
+        signupButton = findViewById(R.id.singup_button);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("users");
+
+                String name = signupName.getText().toString();
+                String email = signupEmail.getText().toString();
+                String username = signupUsername.getText().toString();
+                String password = signupPassword.getText().toString();
+
+                HelperClass helperClass = new HelperClass(name,email,username,password);
+                reference.child(name).setValue(helperClass);
+
+                Toast.makeText(SignupActivity.this,"You have signup succesfull",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignupActivity.this, Log.class);
+                startActivity(intent);
+
+            }
+        });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
